f1b580f51cf3727ae660b3dcbaa70304aecf92fb
Arturo Bringas <17807865+ArturoBG@users.noreply.github.com>
2023-02-04 09:37:28 -0600

    update PlayerMovement
    Move method

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 0.1f;
+    private CharacterController characterController;
+      
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
 
     public void Move(Vector2 input)
     {
-        Debug.Log("Input x: "+input.x+ " y " + input.y );
+        Debug.Log("input x: "+input.x+ " y: " + input.y );
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection.x = input.x;
+        moveDirection.z = input.y;
+
+        characterController.Move(this.transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
     }
 
     public void Fire(InputAction.CallbackContext context)
